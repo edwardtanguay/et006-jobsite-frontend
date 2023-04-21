c60f3086c982e29a33d884e84201dc75ed124e19
Edward Tanguay (windows) <edward@tanguay.info>
2023-04-21 16:37:24 +0200

    backend

--- src/AppContext.tsx
+++ src/AppContext.tsx
@@ -1,13 +1,15 @@
 import { useState, useEffect } from 'react';
 import { createContext } from 'react';
-import { IJob } from './interfaces';
+import { IJob, ISkill } from './interfaces';
 import axios from 'axios';
 
 const jobsUrl = 'https://edwardtanguay.vercel.app/share/jobs.json';
+const skillsUrl = 'http://localhost:3610/skills';
 
 interface IAppContext {
 	appTitle: string;
 	jobs: IJob[];
+	skills: ISkill[];
 }
 
 interface IAppProvider {
@@ -19,6 +21,7 @@ export const AppContext = createContext<IAppContext>({} as IAppContext);
 export const AppProvider: React.FC<IAppProvider> = ({ children }) => {
 	const appTitle = 'The Job Site';
 	const [jobs, setJobs] = useState<IJob[]>([]);
+	const [skills, setSkills] = useState<ISkill[]>([]);
 
 	useEffect(() => {
 		(async () => {
@@ -26,11 +29,18 @@ export const AppProvider: React.FC<IAppProvider> = ({ children }) => {
 		})();
 	}, []);
 
+	useEffect(() => {
+		(async () => {
+			setSkills((await axios.get(skillsUrl)).data);
+		})();
+	}, []);
+
 	return (
 		<AppContext.Provider
 			value={{
 				appTitle,
-				jobs
+				jobs,
+				skills
 			}}
 		>
 			{children}
--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -2,11 +2,12 @@ import { useContext } from 'react';
 import { AppContext } from '../AppContext';
 
 export const PageWelcome = () => {
-  const { jobs } = useContext(AppContext);
+  const { jobs, skills } = useContext(AppContext);
 
 	return (
 		<div className="pageWelcome">
 			<p>There are {jobs.length} jobs.</p>
+			<p>There are {skills.length} skills.</p>
 		</div>
 	);
 };
