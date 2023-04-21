d19a64449d26f3dac643579496adaa7db3ee7ed3
Edward Tanguay (windows) <edward@tanguay.info>
2023-04-21 11:14:42 +0200

    showing jobs

--- src/pages/PageJobs.tsx
+++ src/pages/PageJobs.tsx
@@ -1,7 +1,22 @@
+import { useContext } from 'react';
+import { AppContext } from '../AppContext';
+
 export const PageJobs = () => {
+  const { jobs } = useContext(AppContext);
+
 	return (
 		<div className="pageJobs">
-			<p>This is the jobs page.</p>
+			<div className="jobs">
+				{jobs.map(job => {
+					return (
+						<div className="job">
+							<div className="publicationDate">{job.publicationDate}</div>
+							<div className="title"><a target="_blank" href={job.url}>{job.title}</a> - {job.company}</div>
+							<div className="skillList">{job.skillList}</div>
+						</div>
+					)
+				})}
+		</div>
 		</div>
 	);
 };
