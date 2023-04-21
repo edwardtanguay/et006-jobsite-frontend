6410b23e34ed33b1e15d3d8a507fb10dacb709be
Edward Tanguay (windows) <edward@tanguay.info>
2023-04-21 10:18:28 +0200

    usecontext in

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import { NavLink, Routes, Route, Navigate } from 'react-router-dom';
 import { PageJobs } from './pages/PageJobs';
 import { PageSkills } from './pages/PageSkills';
 import { PageWelcome } from './pages/PageWelcome';
+import { useContext } from 'react';
+import { AppContext } from './AppContext';
 
 function App() {
+  const { appTitle } = useContext(AppContext);
+  
 	return (
 		<div className="App">
-			<h1>Job Site</h1>
+      <h1>{appTitle}</h1>
 			<nav>
 				<NavLink to="/welcome">Welcome</NavLink>
 				<NavLink to="/jobs">Jobs</NavLink>
