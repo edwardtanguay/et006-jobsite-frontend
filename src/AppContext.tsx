35bf514b7df041072a36f963859ce50de66a99b9
Edward Tanguay (windows) <edward@tanguay.info>
2023-04-21 11:00:18 +0200

    fetching jobs in useContext

--- src/AppContext.tsx
+++ src/AppContext.tsx
@@ -1,7 +1,13 @@
+import { useState, useEffect } from 'react';
 import { createContext } from 'react';
+import { IJob } from './interfaces';
+import axios from 'axios';
+
+const jobsUrl = 'https://edwardtanguay.vercel.app/share/jobs.json';
 
 interface IAppContext {
 	appTitle: string;
+	jobs: IJob[];
 }
 
 interface IAppProvider {
@@ -12,11 +18,19 @@ export const AppContext = createContext<IAppContext>({} as IAppContext);
 
 export const AppProvider: React.FC<IAppProvider> = ({ children }) => {
 	const appTitle = 'The Job Site';
+	const [jobs, setJobs] = useState<IJob[]>([]);
+
+	useEffect(() => {
+		(async () => {
+			setJobs((await axios.get(jobsUrl)).data);
+		})();
+	}, []);
 
 	return (
 		<AppContext.Provider
 			value={{
-				appTitle
+				appTitle,
+				jobs
 			}}
 		>
 			{children}
