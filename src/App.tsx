81af691008c5c6309f23cab01f3bfd762686d591
Edward Tanguay (windows) <edward@tanguay.info>
2023-04-21 09:58:22 +0200

    rereact router in

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import './App.scss'
+import './App.scss';
+import { NavLink, Routes, Route, Navigate } from 'react-router-dom';
+import { PageJobs } from './pages/PageJobs';
+import { PageSkills } from './pages/PageSkills';
+import { PageWelcome } from './pages/PageWelcome';
 
 function App() {
-  return (
-    <div className="App">
-    <h1>Job Site</h1>
-    <p>Welcome to this site.</p>
-    </div>
-  )
+	return (
+		<div className="App">
+			<h1>Job Site</h1>
+			<nav>
+				<NavLink to="/welcome">Welcome</NavLink>
+				<NavLink to="/jobs">Jobs</NavLink>
+				<NavLink to="/skills">Skills</NavLink>
+			</nav>
+			<Routes>
+				<Route path="/welcome" element={<PageWelcome />} />
+				<Route path="/jobs" element={<PageJobs />} />
+				<Route path="/skills" element={<PageSkills />} />
+				<Route path="/" element={<Navigate to="/welcome" replace />} />
+			</Routes>
+		</div>
+	);
 }
 
-export default App
+export default App;
